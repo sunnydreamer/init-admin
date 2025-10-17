8869328618c257640ec023b073b8b38cb8860f19
sunnydreamer <sunnylee9516@gmail.com>
2025-10-17 10:20:47 -0400

    env setup

--- fireabse.ts
+++ fireabse.ts
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC8sneYskawP0s0Xmp-C9jY6nGexgkbA6g",
-  authDomain: "init-app-production.firebaseapp.com",
-  projectId: "init-app-production",
-  storageBucket: "init-app-production.firebasestorage.app",
-  messagingSenderId: "1065244984297",
-  appId: "1:1065244984297:web:15776998494c7e08be4178",
-  measurementId: "G-SRZP0BMV81",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID!,
 };
 
 // Initialize Firebase
